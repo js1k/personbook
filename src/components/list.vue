ff58e72eddee989e74467bf5e53d1d07b2e4b40c
bww <wenwu.bao5@transsion.com>
2022-08-14 11:20:19 +0800

    add list

--- src/components/list.vue
+++ src/components/list.vue
@@ -22,13 +22,122 @@
 <script  lang="ts" setup>
 import {ref} from 'vue'
 let arr = ref([])
-arr.value.length = 1000
-arr.value[0] = {
+arr.value = [{
     tit: 'App link',
+    href: 'http://www.baidu.com',
+    date: '2022.08.13',
+    type: 'Call app'
+},{
+    tit: 'Call app',
     href: 'http://www.baidu.com',
     date: '2022.08.14',
     type: 'Call app'
-}
+},{
+    tit: 'performance',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '性能'
+},{
+    tit: 'userAgent',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '浏览器'
+},{
+    tit: 'fetch',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '请求'
+},{
+    tit: '1px',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '上报'
+},{
+    tit: '错误捕获',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '错误捕获'
+},{
+    tit: 'nodejs koa ssr',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: 'ssr'
+},{
+    tit: 'FP & FCP',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '性能'
+},{
+    tit: 'LCP',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '性能'
+},{
+    tit: '性能优化',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '性能'
+},{
+    tit: '开发 JS SDK',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: 'SDK'
+},{
+    tit: '本地缓存',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '优化'
+},{
+    tit: 'uuid',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '浏览器唯一识别'
+},{
+    tit: 'lzString',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '数据压缩'
+},{
+    tit: 'app缓存',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: 'H5优化'
+},{
+    tit: '图片压缩',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: 'H5优化'
+},{
+    tit: 'DNS优化',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '优化'
+},{
+    tit: '图片预加载',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '优化'
+},{
+    tit: 'dom监听',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '功能'
+},{
+    tit: 'web数据链路',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '数据'
+},{
+    tit: '302',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '功能'
+},{
+    tit: '迈过低估，光明将至',
+    href: 'http://www.baidu.com',
+    date: '2022.08.14',
+    type: '心路'
+}]
 
 </script>
 
